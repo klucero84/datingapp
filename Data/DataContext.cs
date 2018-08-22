5f4750e7b48b29c61c5ee954c120349f57855a4d
Kenneth Lucero <Kenneth@KennethLucero.com>
2018-08-22 12:07:39 -0700

    moduel 3. authentication with jwt, hashing with hmac512, authorization and validation with annotations.

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -5,7 +5,10 @@ namespace DatingApp.API.Data
 {
     public class DataContext : DbContext
     {
+
+        public  DbSet<User> Users { get; set; }
         public DbSet<Value> Values { get; set; }
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        
     }
 }
