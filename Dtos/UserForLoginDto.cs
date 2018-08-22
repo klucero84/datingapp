a35124961644d89d1b388d1b5116625a90ac37a1
Kenneth Lucero <Kenneth@KennethLucero.com>
2018-08-22 16:40:38 -0700

    end of module 4

--- Dtos/UserForLoginDto.cs
+++ Dtos/UserForLoginDto.cs
@@ -8,7 +8,6 @@ namespace DatingApp.API.Dtos
         public string Username { get; set; }
 
         [Required]
-        [StringLength(8, MinimumLength = 4, ErrorMessage = "Must be between 4 and 8 characters")]
-        public string Password { get; set; }
+                public string Password { get; set; }
     }
 }
